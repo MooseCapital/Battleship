d863ecab85e914d113e455de414c7a1af084cda3
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2022-11-15 19:21:05 -0600

    no ships overlapping & no coordinates past end, loops created

--- app.js
+++ app.js
@@ -13,20 +13,29 @@ let gameData = (function () {
             let sunk = false;
             let length = Math.floor(Math.random() * 5) + 2;
             let coords = [];
-            let coordNum = Math.floor(Math.random() * 400) + 1;
+           let coordNum = Math.floor(Math.random() * 100) + 1;
+
             let coord = `${typeOfPlayer}${coordNum}` ;
-            //let coordNums = [];
+
 
             // to make it so the ships do not cross over to the next line, we can make it divisible by 20
             //and test if it includes that and the next num in coordNums array
             for (let i = 0; i < length; i++) {
                if (i === 0) {
                     coords.push(coord)
-                    //coordNums.push(coordNum);
+
                } else {
+                    if (coordNum + i > 100) {
+                      return  ship(typeOfPlayer)
+
+                      //break and continue simply stop a ship with length 6, starts at 99. coords would be 99,100.. stop. we need to rerun
+                    }
+
                    coord = `${typeOfPlayer}${coordNum + i}`
                     coords.push(coord);
-                    //coordNums.push(coordNum + i);
+                    //if coord starts at 400 above, and has a length going beyond the grid, it will be impossible to hit it, so we need to rerun
+                    //the function ship(type) if its greater
+                    //regenerate if it matches another ship also
                }
             }
 
@@ -38,17 +47,38 @@ let gameData = (function () {
                 return this.sunk = this.length <= this.timesHit;
             }
 
-            return {length, timesHit, sunk, hit, isSunk, coords}
+            return {coords, length, timesHit, sunk, hit, isSunk}
         }
 
         function gameBoard(typeOfPlayer ) {
                 let ships = [];
                 let Attacks = [];
 
-                for (let i = 0; i < 6; i++) {  //for testing we want 1 ship
+                for (let i = 0; i < 6; i++) {
                     ships.push(ship(typeOfPlayer));
 
+                    for (let ship of ships) {
+                        for (let coord of ship.coords) {
+                            let coordMatch = 0;
+                            //for every coord, we need to loop back through every ship
+                            for (let ship1 of ships) {
+                                for (let coord1 of ship1.coords) {
+
+                                    if (coord === coord1) {
+                                        ++coordMatch
+
+                                        if (coordMatch === 2) {
+                                            return gameBoard(typeOfPlayer)
+                                        }
+                                        //coord will always = coord1 once, because we test it against ourself, we want to see if it hits twice, ANOTHER NUM
+                                    }
+                                }
+                            }
+
+                        }
+                    }
                 }
+
 
                 function allSunk() {
                     //determine if all ships have been sunk, to be called after every attack to check
@@ -97,11 +127,11 @@ let gameData = (function () {
                             }
 
                         }
-                    //console.log(Attacks)
+                    console.log(Attacks)
 
                 }
-                let testCoord = ships[0].coords[0];
-            return { testCoord, receiveAttack, ships, allSunk, Attacks  }
+
+            return {receiveAttack, ships, allSunk, Attacks  }
         }
 
         function player(isComputer = false) {
@@ -123,7 +153,7 @@ let gameData = (function () {
             let computerBoard = gameBoard("B");
             
             function passInClassValue() {
-                //create receiveattack before going further
+                //after generating the coords, test if any ships have the same coords, if so regenerate
             }
 
             return {playerBoard, computerBoard, gameStarted}
@@ -136,6 +166,10 @@ let gameData = (function () {
 let dom = (function () {
     let playerGrid = document.querySelector(".playergrid");
     let computerGrid = document.querySelector(".computergrid");
+    let playerGridDiv = document.querySelectorAll(".playergrid > div");
+    let computerGridDiv = document.querySelectorAll(".computergrid > div");
+    console.log(playerGridDiv)
+    console.log(computerGridDiv)
     let gameCounter = 0;
     let gameLoopDom;
 
@@ -147,7 +181,7 @@ let dom = (function () {
         }
     }
     function computerClick() {
-        let simCoord = `A${Math.floor(Math.random() * 400) + 1}`;
+        let simCoord = `A${Math.floor(Math.random() * 100) + 1}`;
             if (isCoordUsed(simCoord)) {
                 computerClick()
                 return
@@ -160,6 +194,17 @@ let dom = (function () {
         if (e.target.closest(".bottom > button")) {
             gameLoopDom = gameData.gameLoop();
             e.target.innerText = "Reset Game";
+
+            if (e.target.innerText === "Reset Game") {
+                console.log("reset innertext test")
+
+                playerGridDiv.forEach((div) => {
+                    div.className = "";
+                })
+                computerGridDiv.forEach((div) => {
+                    div.className = ""
+                })
+            }
             //disable button for 500ms then settimeout to enable
             //add class to make innertext reset game, then start game when its over
         }
@@ -189,21 +234,21 @@ let dom = (function () {
 
     })
 
-    for (let i = 0; i < 400; i++) {
+    for (let i = 0; i < 100; i++) {
         let div = document.createElement("div");
         let div1 = document.createElement("div");
         // div.classList.add(`A${i + 1}`);
         // div1.classList.add(`B${i + 1}`);
         div.setAttribute("data-coordinate",`A${i + 1}`);
         div1.setAttribute("data-coordinate",`B${i + 1}`);
+        div.className = ""
+        div1.className = ""
         playerGrid.appendChild(div);
         computerGrid.appendChild(div1);
     }
 
     return {}
 })()
-
-
 
 
 
